10ad333c05d162a3ce32a72ae8d51153a8e30148
Ilyasse Lahraoui <ilyasse.lahraoui.cegeka@gmail.com>
2021-02-25 18:42:28 +0100

    hoi

--- GoogleHashCode2021/Program.cs
+++ GoogleHashCode2021/Program.cs
@@ -28,7 +28,7 @@ namespace GoogleHashCode2021
         }
 
 
-
+//hello world
         private static void HandleInput(string input)
         {
             var watch = System.Diagnostics.Stopwatch.StartNew();
